f6ab6d29606f368ee95f0acc6cfcc1c6432514f4
Lavinia Monenciu <Lavinia Monenciu>
2016-05-16 23:25:15 +0300

    Interface Exporter

---
+++ Exporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema.Models
+{
+    public interface Exporter
+    {
+        void export(List<TicketRepository> ticket);
+    }
+}
