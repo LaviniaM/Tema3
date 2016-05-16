ac8c34fc85034cb9848db9f56557900c8c6709cd
Lavinia Monenciu <Lavinia Monenciu>
2016-05-16 23:26:03 +0300

    Factory Exporter

---
+++ ExportFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tema.Models
+{
+    public class ExportFactory
+    {
+        public static Exporter getExporter(int id){
+            if (id==1)
+            {
+               return new CsvExporter();
+
+            }
+            else
+            {
+                if (id==2)
+                {
+                  return  new JsonExporter();
+                }
+            }
+            return null;
+    }
+    }
+}
